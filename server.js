75afbf4a3c0899974fc896e38f4747b3f38689fc
Anh Vuong <vta19797@gmail.com>
2022-05-07 15:22:54 -0500

    rewrite server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { response } = require('express');
 const { json } = require('express/lib/response');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,51 +14,37 @@ const noteDb = JSON.parse(rawData);
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
-
-
-app.get('/api/notes', (req, res) => {
-    res.json(noteDb.slice(1))
-});
-
-
-
+//routes 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-
-function writeNotes(input,res){
-   // const noteInput=input;
-    let tempNote;
-    tempNote.id=Date.now();
-
-    tempNote.title=input.title; 
-    tempNote.text=input.text;
-   
-
-    let attempArray=noteDb;
-    attempArray.push(tempNote);
-    attempArray=JSON.stringify(attempArray);
-
-
-    res.json(fs.writeFile(
-        './db/db.json', attempArray, err => console.log(err)
-    ));
-
-
-
+//api routes
+app.get('/api/notes',(req,res)=>{
+    res.json (noteDb);
 }
-app.post('/notes',(req,res) =>{
-    const noteInput =writeNotes(req.body,res);
-   
+)
+app.post('/api/notes',(req,res)=>{
+    let tempArray = noteDb;
+    let newNote = {};
+    newNote.id=Date.now();
+    newNote.title=req.body.title;
+    newNote.text=req.body.text;
+    tempArray.push(newNote); 
+    tempArray=JSON.stringify(tempArray);
+    res.json(fs.writeFile(
+        "./db/db.json", tempArray,err=>console.log(err)
+    ))
+})
 
-});
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
